d37c1aa7a968021ceb655abe3a46bcfab5004606
Ziloden <ziloden@pm.me>
2025-08-11 09:36:08 -0700

    Add object groups

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
 
     game_clock = pygame.time.Clock()
     dt = 0
@@ -19,8 +22,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(SCREEN_COLOR)
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for game_object in drawable:
+            game_object.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(TICK_DELAY) / 1000
 
